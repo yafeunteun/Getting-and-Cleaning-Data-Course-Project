c431fbe334b597d3a350f2c6931816bac9926066
yafeunteun <yannfeunteun@gmail.com>
2016-01-31 11:49:24 +0000

    Added some comments + added writing result to file

--- run_analysis.R
+++ run_analysis.R
@@ -16,11 +16,11 @@
 #################################################################################
 
 
-## Load required packages
+# Load required packages
 library(dplyr)
 library(data.table)
 
-## Load files in memory 
+# Load files in memory 
 X_test <- fread("./test/X_test.txt")
 y_test <- fread("./test/y_test.txt")
 
@@ -31,6 +31,8 @@ activity_labels <- fread("./activity_labels.txt")
 
 subject_train <- fread("./train/subject_train.txt")
 subject_test <- fread("./test/subject_test.txt")
+
+features <- fread("features.txt")
 
 # Merges the training and the test sets to create one data set.
 X_total <- bind_rows(X_train, X_test)
@@ -47,7 +49,7 @@ y_total <- mutate(y_total, V1=activity_labels$V2[V1])
 rm(activity_labels)
 
 
-features <- fread("features.txt")
+# Extract only the measurements on the mean and standard deviation
 meanLabels <- features[grepl("mean[(]", features$V2),]
 stdLabels <- features[grepl("std[(]", features$V2),]
 temp1 <-  select(X_total, meanLabels$V1) 
@@ -55,23 +57,32 @@ colnames(temp1) <- meanLabels$V2
 temp2 <-  select(X_total, stdLabels$V1) 
 colnames(temp2) <- stdLabels$V2
 
+# Merge mean measurements and std measurements
 X_mean_std_total <- bind_cols(temp1, temp2)
+
+# free memory
 rm(temp1, temp2)
+rm(meanLabels, stdLabels)
 
-
+# Appropriately rename variables
 y_total <- rename(y_total, activity=V1)
 subject_total <- rename(subject_total, subject=V1)
 
-# Order columns by name 
+# Perform some small cleaning
+## subject variable shouldn't be used to do some mathematical operations.
+total$subject <- factor(total$subject)
+## Order columns by name 
 total <- select(total, order(colnames(total)))
 
+# Merge measurements records with corresponding activity labels and subject ids
 total <- bind_cols(subject_total, y_total, X_mean_std_total)
 
 # Create a second, independent tidy data set with the average of each variable for each activity and each subject.
 total_by_activity_subject <- group_by(total, activity,subject) %>% summarise_each(funs(mean))
 
-
-
+# Write resulting data sets to files 
+write.table(total, file="./data_out/mean_and_std_measurements.txt")
+write.table(total_by_activity_subject, file="./data_out/avg_mean_and_std_measurements.txt")
 
 
 
